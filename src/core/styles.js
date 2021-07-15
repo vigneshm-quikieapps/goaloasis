79770a67a6b86d5bd5ab7385a31c36cdd26465ac
DipakRaut <41799246+DipakRaut@users.noreply.github.com>
2021-07-15 18:56:36 +0530

    Added Core File

--- src/core/styles.js
+++ src/core/styles.js
@@ -11,4 +11,22 @@ export const colors = {
 	timelineSkinDarker: "#B3855C",
 }
 
-export const content = {}
+export const content = {
+	My_Milestone:
+		"It looks like you don’t have a plan to achieve your goal yet. Don’t worry! Tap (+) to add a milestone and get on your way.",
+	Tip1: "Tip: Think of milestones as a mini goal that helps you reach your ultimate goal.",
+	Tip2: "Tip: adding a target date will help you stay on track. Don’t worry! You can always change it.",
+	First_Slider:
+		"Achieving a goal requires dedication and hard work. Goal Oasis will help you stay on track for up to six goals.",
+	Second_Slider:
+		"The key to achieving a goal is breaking it down into manageable milestones. We can help you do just that!",
+	Third_Slider:
+		"Progress is made one step at a time! We’ll help keep your daily task list focused on what matters to you.",
+	Fourth_Slider:
+		"Never lose sight of the big picture with a comprehensive view of your goals, milestones, and tasks. You can do it!",
+	Name_Goal:
+		"Be specific, try to put a real and exact figure on it. Make sure it’s achievable so that you build on your momentum.",
+	Whats_Important: "Write out what this goal means to you and make sure it’s something important.",
+	Your_Target_Date:
+		"When your goal is time-bound, it become measurable. Don’t stress over it if you are unsure about the exact date. You can always adjust later.",
+}
