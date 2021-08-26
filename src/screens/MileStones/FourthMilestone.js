f22514c774efc2ab6666f856c71dc802fb3f4969
DipakRaut <41799246+DipakRaut@users.noreply.github.com>
2021-08-26 17:05:46 +0530

    solved back button problem in milestone flow

--- src/screens/MileStones/FourthMilestone.js
+++ src/screens/MileStones/FourthMilestone.js
@@ -269,7 +269,12 @@ const FourthMilestone = ({
 							// style={{backgroundColor: ColorConstants.whiteOp50}}
 							size={50}
 							bottom={0}
-							nextClick={() => navigation.navigate("FirstMilestone")}
+							back
+							prevClick={() =>
+								navigation.navigate("FirstMilestone", {
+									setBackEditScreen: true,
+								})
+							}
 						/>
 						<CommonPrevNextButton
 							right={true}
